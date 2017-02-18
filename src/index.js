1784bbf547107341b88f56c21f7be1ea141f4beb
R_N101 <R_N101@msn.com>
2017-02-18 12:31:05 +0000

    react basics

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+//creating our app and injecting into #root
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
